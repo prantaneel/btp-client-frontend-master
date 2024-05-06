8c19888f0797d129355bd7c5c92ef6099bba6b20
prantaneel <prantaneel2001@gmail.com>
2024-05-07 04:13:30 +0530

    added support for divergent algos

--- src/controlComp/Strategy.js
+++ src/controlComp/Strategy.js
@@ -7,19 +7,27 @@ import ConsensusControls from "./ConsensusControls";
 import TextBox from "../mainComp/TextBox";
 
 export default function Strategy() {
-  const strategyList = ["DIFFUSION_ATC", "DIFFUSION_CTA", "CONSENSUS"];
-  const errorList = ["LMS", "OP2", "OP3"];
+  const strategyList = ["DIFFUSION_ATC", "DIFFUSION_CTA", "CONSENSUS", "INCREMENTAL"];
+  const algoList = ["LMS", "KLLMS", "SLMS", "BLMS", "AALMS"];
 
   const [selectedStrategy, setSelectedStrategy] = useState("DIFFUSION_ATC");
+  const [selectedAlgo, setSelectedAlgo] = useState("LMS");
   const [stepSize, setStepSize] = useState(0);
   const [nodeSize, setNodeSize] = useState(0);
   const [iterations, setIterations] = useState(0);
   const [weightSize, setWeightSize] = useState(0);
   const [processId, setProcessId] = useState("NULL")
   const [lastProcessStrategy, setLastProcessStrategy] = useState("NULL")
+  const [errorProb, setErrorProb] = useState(0)
+  const [beta, setBeta] = useState(1)
+  const [alpha, setAlpha] = useState(0)
+
 
   const receiveStrategyState = (data) => {
     setSelectedStrategy(data);
+  };
+  const receiveAlgoState = (data) => {
+    setSelectedAlgo(data);
   };
   const receiveStepSize = (data) => {
     setStepSize(data);
@@ -32,6 +40,15 @@ export default function Strategy() {
   }
   const receiveWeightSize = (data) => {
     setWeightSize(data);
+  }
+  const receiveErrorProb = (data) => {
+    setErrorProb(data)
+  }
+  const receiveBeta = (data) => {
+    setBeta(data)
+  }
+  const receiveAlpha = (data) => {
+    setAlpha(data)
   }
   const divToRender = [
     <IncrementalControls></IncrementalControls>,
@@ -49,7 +66,7 @@ export default function Strategy() {
       headers:{
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({"stepSize": stepSize, "nodeNumber": nodeSize, "iterations": iterations, "weightSize": weightSize, "strategy": selectedStrategy}),
+      body: JSON.stringify({"stepSize": stepSize, "nodeNumber": nodeSize, "iterations": iterations, "weightSize": weightSize, "strategy": selectedStrategy, "error_prob": errorProb, "algo": selectedAlgo, "alpha": alpha, "beta": beta}),
     }).then((response)=>response.json()).then((data)=>{
       console.log(data)
       return fetch('http://127.0.0.1:8000/train/last_data')
@@ -86,10 +103,19 @@ export default function Strategy() {
           sendData={receiveStrategyState}
           defaultValue={"DIFFUSION_ATC"}
         ></DropDown>
+        <DropDown
+          listName="Algorithm"
+          optionList={algoList}
+          sendData={receiveAlgoState}
+          defaultValue={"LMS"}
+        ></DropDown>
+        {selectedAlgo === "BLMS"? <TextBox fieldName="Beta" sendData={receiveBeta}></TextBox> : null}
+        {selectedAlgo === "AALMS"? <TextBox fieldName="Alpha" sendData={receiveAlpha}></TextBox> : null}
         <TextBox fieldName="Step Size" sendData={receiveStepSize}></TextBox>
         <TextBox fieldName="Number of Nodes" sendData={receiveNodeSize}></TextBox>
         <TextBox fieldName="Number of Iterations" sendData={receiveIterations}></TextBox>
         <TextBox fieldName="Size of W_opt" sendData={receiveWeightSize}></TextBox>
+        <TextBox fieldName="Error Probability" sendData={receiveErrorProb}></TextBox>
         <button className="button-style" onClick={handleTrain}>Train</button>
         <button className="button-style button-red" onClick={handleTrainAbort}>Abort</button>
       </div>
